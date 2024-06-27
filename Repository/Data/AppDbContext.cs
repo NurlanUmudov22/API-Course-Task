534df38ff65ac3a8256d27ae07cd5b1bb8776d12
Umudov Nurlan <nurlanmu@code.local>
2024-06-27 12:48:49 +0400

    add task

---
+++ Repository/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Configurations;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Repository.Data
+{
+    public  class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Group> Groups { get; set; }
+
+
+        public DbSet<Student> Students { get; set; }
+
+
+        public DbSet<GroupStudent> GroupStudents { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StudentConfiguration).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
